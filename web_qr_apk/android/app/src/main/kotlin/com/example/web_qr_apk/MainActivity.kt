d03409ffe964e58529ff7c071ea2c88746bb0473
AdrianAmec <adriancmec@gmail.com>
2026-04-29 13:11:47 +0200

    web

---
+++ web_qr_apk/android/app/src/main/kotlin/com/example/web_qr_apk/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.web_qr_apk
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ web_qr_apk/lib/main.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+import 'dart:html' as html;
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    const apkUrl = "https://raw.githubusercontent.com/AdrianAmec/OceanPark_APP/dev/apk/android-debug.apk";
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: const Color(0xFF020A1A),
+        body: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              // TÍTULO
+              const Text(
+                "OCEAN PARK",
+                style: TextStyle(
+                  color: Color(0xFF6FE7DD),
+                  fontSize: 28,
+                  letterSpacing: 2,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+
+              const SizedBox(height: 30),
+
+              // TEXTO
+              const Text(
+                "Descarga el juego",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white70,
+                  fontSize: 16,
+                ),
+              ),
+
+              const SizedBox(height: 40),
+
+              // QR
+              QrImageView(
+                data: apkUrl,
+                size: 180,
+                backgroundColor: Colors.white,
+              ),
+
+              const SizedBox(height: 30),
+
+              // BOTÓN ABAJO
+              GestureDetector(
+                onTap: () {
+                  html.window.open(apkUrl, "_blank");
+                },
+                child: Container(
+                  width: 300,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    gradient: const LinearGradient(
+                      colors: [
+                        Color(0xFF00C6A7),
+                        Color(0xFF00A896),
+                      ],
+                    ),
+                    borderRadius: BorderRadius.circular(6),
+                  ),
+                  alignment: Alignment.center,
+                  child: const Text(
+                    "DESCARGAR",
+                    style: TextStyle(
+                      color: Colors.white,
+                      letterSpacing: 1,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
